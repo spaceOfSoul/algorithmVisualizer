6fbbe742de5bae26b5f47d1f6d4226cef57c9773
qwer <85132981+ch-tuna@users.noreply.github.com>
2022-05-22 18:55:22 +0900

    main ui

--- app.py
+++ app.py
@@ -1,4 +1,11 @@
 from tkinter import *
 import sortingVisualze
 
-sortingVisualze.main()
+root = Tk()
+root.title("algorithm visualizer")
+root.geometry('400x300')
+
+btn_sort = Button(root, padx=10, pady=50,width=200, height=100, text="Sorting", command=sortingVisualze.main )
+btn_sort.pack()
+
+root.mainloop()
